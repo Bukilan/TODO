05c5ba8b9aceb88925b76af522192cc31b4d0efe
Egor Golod <golod.egor58@gmail.com>
2020-12-02 05:30:10 +0300

    Updates

---
+++ precache-manifest.b23b76b4a76394f476234aed9722e53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bdd8837f6f1510ee153656f8e91afad",
+    "url": "/TODO/index.html"
+  },
+  {
+    "revision": "01f6b36a1a7c0de78b9d",
+    "url": "/TODO/static/css/main.26ddb9ae.chunk.css"
+  },
+  {
+    "revision": "4ec3d1681b3ed8573cbc",
+    "url": "/TODO/static/js/2.2fc610e0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TODO/static/js/2.2fc610e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f6b36a1a7c0de78b9d",
+    "url": "/TODO/static/js/main.163954d4.chunk.js"
+  },
+  {
+    "revision": "6bbabf6c8bcaea96695a",
+    "url": "/TODO/static/js/runtime-main.c03f9dc9.js"
+  }
+]);
